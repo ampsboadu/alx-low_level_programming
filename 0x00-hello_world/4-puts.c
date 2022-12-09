4e274ecfbe3a0f75678329acc1d2d882b31229f8
Ampong Boadu <ampsboadu@gmail.com>
2022-12-08 20:27:37 -0800

    Q4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Output in C without prinf function and Reurn 0
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
+
