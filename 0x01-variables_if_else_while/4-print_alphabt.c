3de9993aeae0ef58f002b82d05530a4c0f968a68
Ampong Boadu <ampsboadu@gmail.com>
2022-12-09 19:46:06 -0800

    Alphabets exclude q & e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -print all aphabets exclude q & e
+ *
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' || a != 'e')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
