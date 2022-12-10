18a04c4908d9ff5988c0cbc8fcabf6ff9f594ce1
Ampong Boadu <ampsboadu@gmail.com>
2022-12-09 20:01:05 -0800

    Print numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -print all single digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d", num);
+num++;
+}
+printf("\n");
+return (0);
+}
