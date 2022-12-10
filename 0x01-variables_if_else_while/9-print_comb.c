ba05876f719b2181363b830aa910fe64e3ff7051
Ampong Boadu <ampsboadu@gmail.com>
2022-12-09 20:38:43 -0800

    Print numbers seperate by ,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -prints numbers seperate by ,
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
