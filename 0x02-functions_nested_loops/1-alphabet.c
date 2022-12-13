2b14a1d9f7ae787f4b47a4d293ed2983576718a5
Ampong Boadu <ampsboadu@gmail.com>
2022-12-13 12:41:13 -0800

    print alphabets in lowercase with function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+/**
+ * print_alphabet -prints alphabets in lower case
+ */
+
+void print_alphabet(void)
+{
+	char alpha 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+	return;
+}
