f182852563a402a20adf64f58bc1c55684f5cdae
Ampong Boadu <ampsboadu@gmail.com>
2022-12-13 13:54:33 -0800

    print alphabets 10x

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,4 +7,16 @@
 
 int _putchar(char c);
 
+/**
+ * print_alphabet -blueprint to print alphabets in lowercase
+ */
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - blueprint to print alphabets 10times
+ */
+
+void print_alphabet_x10(void);
+
 #endif
