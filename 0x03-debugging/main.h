64991c31dfae7c31342a560adec4ad23c37dd77d
Ampong Boadu <ampsboadu@gmail.com>
2022-12-15 06:47:28 -0800

    day remain

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,6 +11,18 @@ void positive_or_negative(int i);
  * largest_number -blueprint for largest number func.
  */
 
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c)
+	
+/**
+ * print_remaining_days - blueprint for print remaing days
+ */
+
+void print_remaining_days(int month, int day, int year);
+
+/**
+ * convert_day -blueprint for day convertor
+ */
+
+int convert_day(int month, int day);
 
 #endif
