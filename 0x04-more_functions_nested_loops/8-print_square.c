c39dcd57cf78b374593a133c441b142534893d4f
Ampong Boadu <ampsboadu@gmail.com>
2022-12-15 21:05:47 -0800

    print square correction 1

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,18 @@ void print_square(int size)
 	int i = 0;
 	int j;
 
-	while (i < size)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
+		while (i < size)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
 		_putchar('\n');
-		i++;
 	}
 }
