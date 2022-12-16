76260af24ecd653da3af8a65f075752db8469f12
Ampong Boadu <ampsboadu@gmail.com>
2022-12-15 21:23:15 -0800

    triangle draw

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle -draws traingle
+ * @size: height of triangle
+ */
+
+void print_triangle(int size)
+{
+	int height;
+	int width;
+	int draw;
+
+	if (size > 0)
+	{
+		for (height = 1; height <= size; height++)
+		{
+			for (width = 1; width <= (size - height); width++)
+				_putchar(' ');
+
+			for (draw = 1; draw <= height; draw++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
