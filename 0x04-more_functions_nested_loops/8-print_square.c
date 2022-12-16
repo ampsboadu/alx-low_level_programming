4153760c21ed20e7f09cd4ea9cf9cf49881c0281
Ampong Boadu <ampsboadu@gmail.com>
2022-12-15 20:56:39 -0800

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square -print square of #
+ * @size: lenght of square
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int j;
+
+	while (i < size)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		putchar('\n');
+		i++;
+	}
+}
