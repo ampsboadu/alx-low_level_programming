359c4b3b7510f07a53a075c8a08e52f5b75a5483
Ampong Boadu <ampsboadu@gmail.com>
2022-12-15 19:35:36 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.c"
+/**
+ * print_line -draws line
+ * @n: lenght of the line
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
