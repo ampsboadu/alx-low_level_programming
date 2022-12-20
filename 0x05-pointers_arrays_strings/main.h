ede2aef62bc3e833606aaf077279c67b18710f95
Ampong Boadu <ampsboadu@gmail.com>
2022-12-20 04:11:26 -0800

    README and others files added

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,70 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _putchar -print character
+ */
+
+int _putchar(char c);
+
+/**
+ * reset_to_98 -update value of pointer to 98
+ */
+
+void reset_to_98(int *n);
+
+/**
+ * swap_int -swap value of two integers
+ */
+
+void swap_int(int *a, int *b);
+
+/**
+ * _strlen -return lenght of string
+ */
+
+int _strlen(char *s);
+
+/**
+ * _puts -prints string to stdout
+ */
+
+void _puts(char *str);
+
+/**
+ * print_rev -prints string in reverse
+ */
+
+void print_rev(char *s);
+
+/**
+ * rev_string -return reverse of string
+ */
+
+void rev_string(char *s);
+
+/**
+ * puts2 -prints other chars of string
+ */
+
+void puts2(char *str);
+
+/**
+ * puts_half -prints half of string
+ */
+
+void puts_half(char *str);
+
+/**
+ * print_array -print elements of int array
+ */
+
+void print_array(int *a, int n);
+
+/**
+ * _strcpy -copy string ponited to by src
+ */
+
+char *_strcpy(char *dest, char *src);
+
+#endif
