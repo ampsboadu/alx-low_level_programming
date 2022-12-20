7ba4e4d732cf52ae3913e640788b7af7b8de0758
Ampong Boadu <ampsboadu@gmail.com>
2022-12-20 04:48:48 -0800

    String lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen -return len of string
+ * @s: string array
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int i, len;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		len = i + 1;
+	}
+
+	return (len);
+}
