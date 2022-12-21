3467527e35be25cef56612062c5254ab613d3e4c
Ampong Boadu <ampsboadu@gmail.com>
2022-12-20 19:02:38 -0800

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array -prints items in array
+ * @a: array
+ * @n: number of items in a
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == n - 1)
+			printf("%d", a[i]);
+		else
+			printf("%d, ", a[i]);
+
+		i++;
+	}
+}
