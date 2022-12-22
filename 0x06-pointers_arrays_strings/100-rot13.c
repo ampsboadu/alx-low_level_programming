fceb2c680c72a85e6c9363e0da85e522f30630ad
Ampong Boadu <ampsboadu@gmail.com>
2022-12-22 04:11:48 -0800

    Advance task rot-13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rot13 -rot 13 exchange function
+ * @s: input string
+ * Return: string
+ */
+
+char *rot13(char *s)
+{
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+		'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D',
+		'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+	int i;
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i])
+			i++;
+
+		if (s[j] == a[i])
+			s[j] = b[i];
+
+		j++;
+	}
+
+	return (s);
+}
