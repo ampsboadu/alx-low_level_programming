00dd56238adf0f65587ccbc3b80982d4a7d302ff
Ampong Boadu <ampsboadu@gmail.com>
2023-01-03 08:53:03 -0800

    fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	if (n > 0)
+	int len = n;
+
+	if (len > 0)
 	{
 		int i;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < len; i++)
 			s[i] = b;
 	}
 
