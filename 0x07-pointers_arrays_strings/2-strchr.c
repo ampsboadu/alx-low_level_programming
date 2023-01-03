a13b09aa0d39d4d0c9959d32436dd2c849d1db46
Ampong Boadu <ampsboadu@gmail.com>
2023-01-03 09:28:42 -0800

    char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr -finds firts occurance of char in str
+ * @s: string to search
+ * @c: target characer
+ * Return: pointer to char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
+}
