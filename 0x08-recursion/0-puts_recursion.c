6bb2b074c0c7fc9d0e90ac971bb7d18997ac3230
Ampong Boadu <ampsboadu@gmail.com>
2023-01-05 10:36:12 -0800

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,12 +2,12 @@
 
 /**
  * _puts_recursion -prints string
- * @c: string to print
+ * @s: string to print
  */
 
 void _puts_recursion(char *s)
 {
-	if (s[i] == '\0')
+	if (s[0] == '\0')
 	{
 		_putchar('\n');
 		return;
