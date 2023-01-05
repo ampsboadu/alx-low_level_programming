a6e0d9f4b6b881f510768fb674d346e4769ca4ed
Ampong Boadu <ampsboadu@gmail.com>
2023-01-05 10:20:19 -0800

    Readme file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,52 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _putchar -blueprnt to put char func
+ */
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion -blueprint to print strings func
+ */
+
+void _puts_recursion(char *s);
+
+/**
+ * _print_rev_recursion -blueprint to print strings rev func
+ */
+
+void _print_rev_recursion(char *s);
+
+/**
+ * _strlen_recursion -blueprint to string len func
+ */
+
+int _strlen_recursion(char *s);
+
+/**
+ * factorial -blueprint to factorial func
+ */
+
+int factorial(int n);
+
+/**
+ * _pow_recursion -blueprint to power func
+ */
+
+int _pow_recursion(int x, int y);
+
+/**
+ * _sqrt_recursion -blueprint to sqrt func
+ */
+
+int _sqrt_recursion(int n);
+
+/**
+ * is_prime_number -blueprint to is prime func
+ */
+
+int is_prime_number(int n);
+
+#endif
