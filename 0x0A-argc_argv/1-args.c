5d1d4894e72452bb133ed2d7362eeaa017a6802d
Ampong Boadu <ampsboadu@gmail.com>
2023-01-09 03:56:45 -0800

    print number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -prints count of args
+ * @argc: count of args
+ * @argv: array of args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
