5d9a57ca6d0e8560c25d6fbbcfd12933b54abdb3
Ampong Boadu <ampsboadu@gmail.com>
2023-01-09 06:16:59 -0800

    print num of args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,10 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
 }
