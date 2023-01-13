e976b7055ee5f1b71d3f6e6eddfb7e48d79abc53
Ampong Boadu <ampsboadu@gmail.com>
2023-01-12 18:52:02 -0800

    README and main header files

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,34 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _putchar -print character
+ */
+
+int _putchar(char c);
+
+/**
+ * malloc_checked -prototype to malloc_checked func
+ */
+
+void *malloc_checked(unsigned int b);
+
+/**
+ * string_nconcat -prototype to string_nconcat func
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/**
+ * _calloc -prototype to _calloc func
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/**
+ * array_range -prototype to array_range func
+ */
+
+int *array_range(int min, int max);
+
+#endif
