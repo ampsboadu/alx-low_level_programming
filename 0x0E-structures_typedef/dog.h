6f76689a37998acf082ba6231e15b7e1c787df3a
Ampong Boadu <ampsboadu@gmail.com>
2023-01-16 17:31:08 -0800

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -structure for storing dogs data
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
