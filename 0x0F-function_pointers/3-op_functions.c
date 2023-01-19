753a01c8f13f5d7ccddec5a7635eaf37c09bbccf
Ampong Boadu <ampsboadu@gmail.com>
2023-01-18 21:07:47 -0800

    Operation functions created

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add -addition operation
+ * @a: first num
+ * @b: second num
+ * Return: sum a & b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub -substraction operation
+ * @a: first num
+ * @b: second num
+ * Return: b substracted from a
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul -multiplication operation
+ * @a: first num
+ * @b: second num
+ * Return: a multiply by b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div -division operation
+ * @a: first num
+ * @b: second num
+ * Return: a divided by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -modulo operation
+ * @a: first num
+ * @b: second num
+ * Return: remainder of a divided b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
