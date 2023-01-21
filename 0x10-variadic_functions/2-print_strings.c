408e72791f1bf0b7f7b5c73b7218c3365069028c
Ampong Boadu <ampsboadu@gmail.com>
2023-01-21 00:03:12 -0800

    Print Strings with Sep

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings -print string args separated by sep
+ * @separator: separator
+ * @n: number of args
+ * @...: variable args
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *s;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+	while (i < n)
+	{
+		s = va_arg(args, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		i++;
+		if (i < n && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
