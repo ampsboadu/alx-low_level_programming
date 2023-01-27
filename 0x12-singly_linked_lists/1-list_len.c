857aabbc2821290adac633313f7cb0c70f43cec0
Ampong Boadu <ampsboadu@gmail.com>
2023-01-26 21:35:39 -0800

    Number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,17 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elements of linked list
+ * list_len - prints length of list_t linked list
  * @h: linked list
- * Return: number of nodes in linked list
+ * Return: number of nodes/elements (length) in linked list
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
 		count++;
 		h = h->next;
 	}
