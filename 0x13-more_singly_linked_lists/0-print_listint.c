08d7c963356bdeb9876eb4efd86f2e1ceb3b57de
Ampong Boadu <ampsboadu@gmail.com>
2023-02-01 04:08:36 -0800

    Correction to first question

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+ * printnumber -print integer with _putchar
+ * @n: integer to print
+ */
+
+void printnumber(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n == 0)
+		_putchar('0');
+
+	if (n / 10)
+		printnumber(n / 10);
+
+	_putchar(n % 10 + '0');
+}
+
+/**
  * print_listint - prints all elements of a list
  * @h: list
  * Return: number of nodes
@@ -12,7 +34,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n); /* autofills 0 if no int value given */
+		/* printf("%d\n", h->n);  autofills 0 if no int value given */
+		printnumber(h->n);
+		_putchar('\n');
 
 		num_nodes += 1;
 		h = h->next;
