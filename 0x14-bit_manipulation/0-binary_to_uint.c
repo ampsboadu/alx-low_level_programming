69b82e666ab2f11c51193d152c896b031c17c91c
Ampong Boadu <ampsboadu@gmail.com>
2023-02-03 06:53:10 -0800

    Bin to Dec concert

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: binary string
+ * Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len = 0, pow = 1;
+	unsigned int sum = 0, error = 0;
+
+	if (b == NULL)
+		return (error);
+
+	while (b[len] != '\0')
+		len++;
+	len -= 1;
+
+	while (len >= 0)
+	{
+		if ((b[len] != '0') && (b[len] != '1'))
+			return (error);
+
+		if (b[len] == '1')
+			sum += pow;
+
+		pow *= 2;
+		len--;
+	}
+	return (sum);
+}
