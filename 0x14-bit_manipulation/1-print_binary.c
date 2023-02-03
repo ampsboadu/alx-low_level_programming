fa2d39f9e1918ab1ec0d4f5ff50f92947dfbb430
Ampong Boadu <ampsboadu@gmail.com>
2023-02-03 07:20:09 -0800

    convert and print bin

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary -converts unsigned int to binary
+ * @n: int to convert
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int copy = n, mask = 1;
+	int len = 0;
+
+	while (copy > 0)
+	{
+		len++;
+		copy >>= 1;
+	}
+	len -= 1;
+
+	while (len >= 0)
+	{
+		if ((n >> len) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		len--;
+	}
+}
