9f4559a46cc9b7b492deb2de147051ceaa639e72
Ampong Boadu <ampsboadu@gmail.com>
2023-03-21 00:40:43 -0700

    Advance task

---
+++ 0x18-dynamic_libraries/funcs.c
@@ -0,0 +1,54 @@
+/**
+ * add -add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub -substract number from number
+ * @a: first number
+ * @b: second number
+ * Return: a-b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul -multiply two numbers
+ * @a: fisrt numebr
+ * @b: second number
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div -divide number
+ * @a: first number
+ * @b: second number
+ * Return: a/b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod -modulo of number
+ * @a: first number
+ * @b: second number
+ * Return: a%b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
